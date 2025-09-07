f4bbb7d7c7c7cbda42cbfc1738196c89aa859e73
Neil Jeffries <neiljeffries@gmail.com>
2025-09-07 00:04:17 -0400

    scripting

--- my-grafana-prometheus-project/scripts/clean-volumes.bat
+++ my-grafana-prometheus-project/scripts/clean-volumes.bat
@@ -3,8 +3,8 @@ setlocal
 pushd "%~dp0.."
 
 echo Removing Grafana and Prometheus named volumes ...
-podman volume rm -f my-podman-compose-project_prometheus-data 2>nul
-podman volume rm -f my-podman-compose-project_grafana-data 2>nul
+podman volume rm -f my-grafana-prometheus-project_prometheus-data 2>nul
+podman volume rm -f my-grafana-prometheus-project_grafana-data 2>nul
 set ERR=%ERRORLEVEL%
 
 popd
